9771b10b6c891c5c0927535427a6b96a7b3267b6
sahilmalawde <161219991+sahilmalawde@users.noreply.github.com>
2024-04-17 16:18:20 +0530

    Update manage.js

--- manage.js
+++ manage.js
@@ -60,7 +60,7 @@ document.getElementById("approve").addEventListener('click',(e)=>{
     var opt = document.getElementById('opt').value;
     get(child(ref(database),'books/'+opt))
     .then((snapshot)=>{
-    var qty = data.qty
+    var qty = data.Qty
     var data = snapshot.val();
     const userid = data.user
     var updates = {
@@ -68,7 +68,7 @@ document.getElementById("approve").addEventListener('click',(e)=>{
         user : "",
         doi:'',
         dor:'',
-        qty : qty++;
+        Qty : qty++;
         
       }
       
